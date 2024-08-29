5273c49a200170d9425f53161db655a4027a8c9f
Vivek Kumar <akavivekkumar@gmail.com>
2024-08-29 19:51:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+from art import logo
+import random
+
+def deal_cards():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) < 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+def compare(u_score, c_score):
+    if u_score == c_score:
+        return "Draw 🙃"
+    elif c_score == 0:
+        return "Loose, Opponent has Blackjack 😎"
+    elif u_score == 0:
+        return "Win with a Blackjack 😎"
+    elif u_score > 21:
+        return "You went over. You loose 😭"
+    elif c_score >21:
+        return "Opponent went over. You Win 😁"
+    elif u_score > c_score:
+        return "You Win 😁"
+    else:
+        return "You Loose 😭"
+
+
+def play_game():
+    print(logo)
+    user_cards = []
+    computer_cards = []
+    computer_score = -1
+    user_score = -1
+    is_game_over = False
+
+    for _ in range(2):
+        user_cards.append(deal_cards())
+        computer_cards.append(deal_cards())
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"Your cards: {user_cards}, Current score: {user_score}")
+        print(f"Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input("Type 'y' to get another card, Type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_cards())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_cards())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"Your final hand: {user_cards}, final score {user_score}")
+    print(f"Computer's final hand: {computer_cards}, final score {computer_score}")
+    print(compare(user_score, computer_score))
+
+while input("Do you want to play a game Blackjack? Type 'y' or 'n': ") =="y":
+    print("\n" * 40)
+    play_game()
+
+
+
+
+
+
+
